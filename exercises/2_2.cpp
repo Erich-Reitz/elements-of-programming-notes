082d0d62d251ec0842a879552ac91b374183b53a
Erich Reitz <erich.reitz@protonmail.com>
2024-05-21 16:33:45 -0400

    lint

--- exercises/2_2.cpp
+++ exercises/2_2.cpp
@@ -1,32 +1,10 @@
 // Exercise: Design an algorithm that determines, given a transformation and its
 // definition-space predicate, whether the orbits of two elements intersect
 
-#include <concepts>
-#include <functional>
-#include <iostream>
-#include <numeric>
-#include <type_traits>
-
 #include "OutOfDefinitionSpace.hpp"
+#include "m_named_traits.hpp"
 
-using DistanceType = int;
-
-template <typename function_t, typename type_p>
-concept is_homogeneous_unary_function = requires(function_t f, type_p a) {
-  { std::invoke(f, a) } -> std::same_as<type_p>;
-};
-
-template <typename Function, typename Type>
-concept is_predicate_function = requires(Function f, Type a) {
-  { std::invoke(f, a) } -> std::convertible_to<bool>;
-};
-
-template <typename Struct, typename Type>
-concept Transformation = requires(Struct s, Type t) {
-  { s.func } -> is_homogeneous_unary_function<Type>;
-  { s.pred } -> is_predicate_function<Type>;
-  { s(t) } -> std::convertible_to<Type>;
-};
+#include <iostream>
 
 struct OrbitModuloTenOperation {
   std::function<int(int)> func = [](int n) -> int {
@@ -49,12 +27,6 @@ private:
     return func(n);
   }
 };
-
-template <typename F, typename P>
-  requires Transformation<F, P>
-P apply_transformation(F transformation, P param) {
-  return transformation(param);
-}
 
 int main() {
   OrbitModuloTenOperation operation;
---
+++ exercises/m_named_traits.hpp
@@ -0,0 +1,26 @@
+#include <concepts>
+#include <functional>
+#include <type_traits>
+
+template <typename function_t, typename type_p>
+concept is_homogeneous_unary_function = requires(function_t f, type_p a) {
+  { std::invoke(f, a) } -> std::same_as<type_p>;
+};
+
+template <typename Function, typename Type>
+concept is_predicate_function = requires(Function f, Type a) {
+  { std::invoke(f, a) } -> std::convertible_to<bool>;
+};
+
+template <typename Struct, typename Type>
+concept Transformation = requires(Struct s, Type t) {
+  { s.func } -> is_homogeneous_unary_function<Type>;
+  { s.pred } -> is_predicate_function<Type>;
+  { s(t) } -> std::convertible_to<Type>;
+};
+
+template <typename F, typename P>
+  requires Transformation<F, P>
+P apply_transformation(F transformation, P param) {
+  return transformation(param);
+}
